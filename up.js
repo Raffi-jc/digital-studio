44700b5c10ec465df38fc78c674cd0d43faba3da
Raffi-jc <161599644+Raffi-jc@users.noreply.github.com>
2024-10-24 09:14:11 +0200

    yuh.js

--- up.js
+++ up.js
@@ -1,3 +1,12 @@
+// Debounce function
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+}
+
 function colorModeToggle() {
   function attr(defaultVal, attrVal) {
     const defaultValType = typeof defaultVal;
@@ -11,7 +20,6 @@ function colorModeToggle() {
 
   const htmlElement = document.documentElement;
   const computed = getComputedStyle(htmlElement);
-  let toggleEl;
   let togglePressed = "false";
 
   const scriptTag = document.querySelector("[tr-color-vars]");
@@ -24,7 +32,7 @@ function colorModeToggle() {
   let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
 
   const cssVariables = scriptTag.getAttribute("tr-color-vars");
-  if (!cssVariables || !cssVariables.length) {
+  if (!cssVariables.length) {
     console.warn("Value of tr-color-vars attribute not found");
     return;
   }
@@ -32,12 +40,12 @@ function colorModeToggle() {
   let lightColors = {};
   let darkColors = {};
   cssVariables.split(",").forEach(function (item) {
-    let lightValue = computed.getPropertyValue(`--color--${item}`);
-    let darkValue = computed.getPropertyValue(`--dark--${item}`);
+    let lightValue = computed.getPropertyValue('--color--${item}');
+    let darkValue = computed.getPropertyValue('--dark--${item}');
     if (lightValue.length) {
       if (!darkValue.length) darkValue = lightValue;
-      lightColors[`--color--${item}`] = lightValue;
-      darkColors[`--color--${item}`] = darkValue;
+      lightColors['--color--${item}'] = lightValue;
+      darkColors['--color--${item}'] = darkValue;
     }
   });
 
@@ -47,7 +55,7 @@ function colorModeToggle() {
   }
 
   function setColors(colorObject, animate) {
-    if (typeof gsap !== "undefined" && gsap.to && animate) {
+    if (typeof gsap !== "undefined" && animate) {
       gsap.to(htmlElement, {
         ...colorObject,
         duration: colorModeDuration,
@@ -60,57 +68,201 @@ function colorModeToggle() {
     }
   }
 
+  function animateHeroElements(dark) {
+    const introBackgroundDark = document.querySelector(".intro_background_dark");
+    const splashHeroDark = document.querySelector(".splash_hero-dark");
+
+    // Only run the animation if these elements exist on the page
+    if (introBackgroundDark && splashHeroDark) {
+      // Handle setting display before the animation starts
+      if (dark) {
+        introBackgroundDark.style.display = "block";
+      }
+
+      gsap.to(
+        [
+          ".splash_hero-dark",
+          ".hero_dark-mode",
+          ".intro_background_dark",
+          ".splash_hero-light",
+          ".hero_light-mode",
+          ".intro_background_light",
+        ],
+        {
+          opacity: (i) => (dark ? (i < 3 ? 1 : 0) : i < 3 ? 0 : 1),
+          duration: colorModeDuration,
+          ease: colorModeEase,
+          onComplete: () => {
+            if (!dark) {
+              introBackgroundDark.style.display = "none";
+            }
+          },
+        }
+      );
+
+      // Handle .is-glow elements
+      gsap.to(".is-glow", {
+        opacity: dark ? 1 : 0,
+        duration: colorModeDuration,
+        ease: colorModeEase,
+      });
+    }
+  }
+
   function goDark(dark, animate) {
+    const introBackgroundDark = document.querySelector(".intro_background_dark");
+    const splashHeroDark = document.querySelector(".splash_hero-dark");
+
+    // Only animate hero elements if they exist on the page
+    if (introBackgroundDark && splashHeroDark) {
+      animateHeroElements(dark);
+    }
+
+    const loadingAnimation = document.querySelector('.loading_animation');
+
     if (dark) {
       localStorage.setItem("dark-mode", "true");
       htmlElement.classList.add("dark-mode");
       setColors(darkColors, animate);
       togglePressed = "true";
+
+      // Change mix-blend-mode to 'darken' in dark mode
+      if (loadingAnimation) {
+        loadingAnimation.style.mixBlendMode = 'darken';
+      }
+
     } else {
       localStorage.setItem("dark-mode", "false");
       htmlElement.classList.remove("dark-mode");
       setColors(lightColors, animate);
       togglePressed = "false";
+
+      // Reset mix-blend-mode to default in light mode
+      if (loadingAnimation) {
+        loadingAnimation.style.mixBlendMode = 'screen';
+      }
     }
-    if (typeof toggleEl !== "undefined") {
-      toggleEl.forEach(function (element) {
-        element.setAttribute("aria-pressed", togglePressed);
-      });
-    }
+    window.dispatchEvent(new Event('colorModeToggle'));
   }
 
-  function checkPreference(e) {
+  // Debounced function for handling media query changes
+  const debouncedCheckPreference = debounce((e) => {
     goDark(e.matches, false);
-  }
+  }, 300);
 
   const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
-  if (colorPreference.addEventListener) {
-    colorPreference.addEventListener("change", (e) => checkPreference(e));
-  } else if (colorPreference.addListener) {
-    colorPreference.addListener((e) => checkPreference(e));
-  }
+  colorPreference.addEventListener("change", debouncedCheckPreference);
 
-  let storagePreference = localStorage.getItem("dark-mode");
-  if (storagePreference !== null && (storagePreference === "true" || storagePreference === "false")) {
-    storagePreference === "true" ? goDark(true, false) : goDark(false, false);
-  } else {
-    checkPreference(colorPreference);
+  function simulateHover(button) {
+    if (button) {
+      const mouseEnterEvent = new Event("mouseenter");
+      button.dispatchEvent(mouseEnterEvent);
+    }
   }
 
   window.addEventListener("DOMContentLoaded", (event) => {
-    toggleEl = document.querySelectorAll("[tr-color-toggle]");
-    toggleEl.forEach(function (element) {
-      element.setAttribute("aria-label", "View Dark Mode");
-      element.setAttribute("role", "button");
-      element.setAttribute("aria-pressed", togglePressed);
-    });
-    document.addEventListener("click", function (e) {
-      const targetElement = e.target.closest("[tr-color-toggle]");
-      if (targetElement) {
-        let darkClass = htmlElement.classList.contains("dark-mode");
-        darkClass ? goDark(false, true) : goDark(true, true);
+    const lightButton = document.querySelector(".light-button");
+    const darkButton = document.querySelector(".dark-button");
+    const modeToggleBtn = document.querySelector(".mode-toggle-btn"); // New toggle button
+
+    let storagePreference = localStorage.getItem("dark-mode");
+    if (storagePreference !== null) {
+      if (storagePreference === "true") {
+        goDark(true, false);
+        simulateHover(darkButton);
+      } else {
+        goDark(false, false);
+        simulateHover(lightButton);
       }
-    });
+    } else {
+      goDark(colorPreference.matches, false);
+    }
+
+    if (lightButton) {
+      lightButton.addEventListener("click", () => {
+        goDark(false, true);
+      });
+
+      lightButton.addEventListener("mouseenter", () => {
+        setColors(lightColors, true);
+        gsap.to(".splash_hero-light, .intro_background_light", {
+          opacity: 1,
+          duration: colorModeDuration,
+          ease: colorModeEase,
+        });
+        gsap.to(".splash_hero-dark, .intro_background_dark", {
+          opacity: 0,
+          duration: colorModeDuration,
+          ease: colorModeEase,
+        });
+      });
+
+      lightButton.addEventListener("mouseleave", () => {
+        const darkClass = htmlElement.classList.contains("dark-mode");
+        if (darkClass) {
+          setColors(darkColors, true);
+          gsap.to(".splash_hero-light, .intro_background_light", {
+            opacity: 0,
+            duration: colorModeDuration,
+            ease: colorModeEase,
+          });
+          gsap.to(".splash_hero-dark, .intro_background_dark", {
+            opacity: 1,
+            duration: colorModeDuration,
+            ease: colorModeEase,
+          });
+        } else {
+          setColors(lightColors, true);
+        }
+      });
+    }
+
+    if (darkButton) {
+      darkButton.addEventListener("click", () => {
+        goDark(true, true);
+      });
+
+      darkButton.addEventListener("mouseenter", () => {
+        setColors(darkColors, true);
+        gsap.to(".splash_hero-dark, .intro_background_dark", {
+          opacity: 1,
+          duration: colorModeDuration,
+          ease: colorModeEase,
+        });
+        gsap.to(".splash_hero-light, .intro_background_light", {
+          opacity: 0,
+          duration: colorModeDuration,
+          ease: colorModeEase,
+        });
+      });
+
+      darkButton.addEventListener("mouseleave", () => {
+        const darkClass = htmlElement.classList.contains("dark-mode");
+        if (darkClass) {
+          setColors(darkColors, true);
+        } else {
+          setColors(lightColors, true);
+          gsap.to(".splash_hero-dark, .intro_background_dark", {
+            opacity: 0,
+            duration: colorModeDuration,
+            ease: colorModeEase,
+          });
+          gsap.to(".splash_hero-light, .intro_background_light", {
+            opacity: 1,
+            duration: colorModeDuration,
+            ease: colorModeEase,
+          });
+        }
+      });
+    }
+
+    // New mode-toggle-btn functionality
+    if (modeToggleBtn) {
+      modeToggleBtn.addEventListener("click", () => {
+        const isDarkMode = localStorage.getItem("dark-mode") === "true";
+        goDark(!isDarkMode, true);
+      });
+    }
   });
 }
 
