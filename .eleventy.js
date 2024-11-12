867cce4b803881a31bb76e17cfbede4932b66bbc
Raffi-jc <raphael.calo@gmail.com>
2024-11-12 14:52:25 +0200

    cms update

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("webflow/images");
     eleventyConfig.addPassthroughCopy("webflow/js");
     eleventyConfig.addPassthroughCopy("webflow/videos");
+    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("static"); // For images or other static content
+  
+    // Add markdown cms processing (e.g., for blog posts)
+    eleventyConfig.addCollection("posts", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("./cms/projects/*.md");
+    });
   
     return {
       dir: {
